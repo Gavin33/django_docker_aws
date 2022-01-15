054d9c9584abca9bb69cbd8731f53388430f7edf
Gavin Butler <GavinButler_33@msn.com>
2022-01-14 22:46:42 -0500

    last try

--- app/hello_django/settings.py
+++ app/hello_django/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-rd_q8=#5%)xlhl8!b8o410apqror1(3^2j6m3jr#xr&!+!8%84
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'http://015658131674.dkr.ecr.us-east-1.amazonaws.com/django_docker_aws:8000']
+ALLOWED_HOSTS = ['http://015658131674.dkr.ecr.us-east-1.amazonaws.com/django_docker_aws:8000', '0.0.0.0', 'localhost', '127.0.0.1']
 #ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
 # Application definition
